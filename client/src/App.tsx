0adad04bc341d36826cc3bbd566583bc2b221109
Pritam Kundu <75939390+warmachine028@users.noreply.github.com>
2024-11-07 05:33:49 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -41,7 +41,7 @@ const App = () => {
 	const queryClient = new QueryClient()
 	return (
 		<QueryClientProvider client={queryClient}>
-			<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
 				<ReactQueryDemoApp />
 			</ThemeProvider>
 		</QueryClientProvider>
